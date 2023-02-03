161bafddb4e46ba0d2c4175ce59902c678ac2153
Tassadar921 <paul.lecuisinier@gmail.com>
2023-02-03 16:37:10 +0100

    fuck les paths

--- back/modules/languages/languages.ts
+++ back/modules/languages/languages.ts
@@ -3,17 +3,17 @@ import { Response } from "express";
 export module languages{
     //sends the json index of languages
     export async function getLanguagesList(res : Response){
-        // @ts-ignore
-        const languagesList = await import('../../files/json/translation/languagesList.json', {assert: {type: 'json'}})
-        console.log(languagesList);
+        console.log('ici');
+        const languagesList = await import('./files/json/translation/languagesList.json', {assert: {type: 'json'}})
         await res.json(languagesList.data);
         return;
     }
 
     //sends the json of the language id selectedLanguage
     export async function getDictionary (language : string, res : Response) {
+        console.log('là');
         // @ts-ignore
-        const translation = await import('../files/json/translation/' + language + '.json', {assert: {type: 'json'}})
+        const translation = await import('files/json/translation/' + language + '.json', {assert: {type: 'json'}})
         await res.json(translation.default);
         return;
     }
