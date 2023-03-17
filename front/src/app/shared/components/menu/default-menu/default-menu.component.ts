f922b9e43996c9aafd71e8cadf464c756dba551c
Paul Cuisinier <80831213+Tassadar921@users.noreply.github.com>
2023-03-17 12:04:36 +0100

    menu v7

--- front/src/app/shared/components/menu/default-menu/default-menu.component.ts
+++ front/src/app/shared/components/menu/default-menu/default-menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import {CookieService} from "../../../services/cookie.service";
 import {DevicePlatformService} from "../../../services/device-platform.service";
 import {Router} from "@angular/router";
@@ -8,10 +8,9 @@ import {Router} from "@angular/router";
   templateUrl: './default-menu.component.html',
   styleUrls: ['./default-menu.component.scss'],
 })
-export class DefaultMenuComponent implements OnInit {
+export class DefaultMenuComponent implements AfterViewInit {
 
   @Input() menuItems: any[] = [];
-  @Input() signOut!: Promise<void>;
 
   constructor(
     public cookieService: CookieService,
@@ -19,6 +18,30 @@ export class DefaultMenuComponent implements OnInit {
     public router: Router
   ) {}
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    for(const menuItem of this.menuItems) {
+      let menuItemElement = document.getElementById('menuItem'+this.menuItems.indexOf(menuItem));
+      menuItemElement!.addEventListener('pointerenter', () => {
+        let left = 0;
+        for(let i = 0; i<this.menuItems.indexOf(menuItem); i++) {
+            left+=document.getElementById('menuItem'+i)!.offsetWidth;
+        }
+        left+= menuItemElement!.offsetWidth/2
+          - document.getElementById('indicator')!.offsetWidth/2;
+        document.getElementById('indicator')!.style.left =
+          left+'px';
+        document.getElementById('indicator')!.style.background =
+          'linear-gradient(130deg, '+this.randomColor()+', '+this.randomColor()+')';
+        });
+    }
+  }
 
+  //function that returns a random color
+  public randomColor() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+
+  @Input() public async signOut() {
+    console.log('ici')
+  }
 }
