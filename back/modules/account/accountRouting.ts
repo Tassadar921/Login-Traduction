782b02cf3f6f956991f2ff261f059615d90f1233
melian-e <iemelian.rambeau@student.junia.com>
2023-04-29 21:58:58 +0200

    bug fix

--- back/modules/account/accountRouting.ts
+++ back/modules/account/accountRouting.ts
@@ -38,7 +38,7 @@ module accountRouting {
 
         app.post('/signUp', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`signUp, ${req.body.username}, ${req.body.password}, ${req.body.email}, ${req.body.language}`);
+                logger.logger.info(`signUp, { username : ${req.body.username}, email :  ${req.body.email}, language : ${req.body.language}}`);
                 await accountSignUp.createUserCreation(req.body.username, req.body.password, req.body.email, req.body.language, res);
             } catch (error) {
                 next(error);
@@ -47,7 +47,7 @@ module accountRouting {
 
         app.post('/confirmSignUp', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`confirmSignUp, ${req.body.urlToken}`);
+                logger.logger.info(`confirmSignUp, { urlToken : ${req.body.urlToken}}`);
                 await accountSignUp.createUser(req.body.urlToken, res);
             } catch (error) {
                 next(error);
@@ -58,7 +58,7 @@ module accountRouting {
 
         app.post('/checkSession', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`checkSession, ${req.body.username}, ${req.body.sessionToken}`);
+                logger.logger.info(`checkSession, { username : ${req.body.username}, sessionToken : ${req.body.sessionToken}}`);
                 if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
                     await res.json({status: 1});
                 }
@@ -72,7 +72,7 @@ module accountRouting {
 
         app.post('/signIn', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`signIn, ${req.body.identifier}, ${req.body.password}`);
+                logger.logger.info(`signIn, { identifier : ${req.body.identifier}}`);
                 await accountSignIn.signIn(req.body.identifier, req.body.password, res);
             } catch (error) {
                 next(error);
@@ -82,7 +82,7 @@ module accountRouting {
 
         app.post('/signOut', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`signOut, ${req.body.username}, ${req.body.sessionToken}`)
+                logger.logger.info(`signOut, {username : ${req.body.username}, sessionToken : ${req.body.sessionToken}}`)
                 await accountSignIn.signOut(req.body.username, req.body.sessionToken, res);
             } catch (error) {
                 next(error);
@@ -93,7 +93,7 @@ module accountRouting {
 
         app.post('/resetPassword', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`resetPassword, ${req.body.email}, ${req.body.language}`);
+                logger.logger.info(`resetPassword, { email : ${req.body.email}, language : ${req.body.language}}`);
                 await accountResetPassword.mailResetPasswordCreateUrlToken(req.body.email, req.body.language, res);
             } catch (error) {
                 next(error);
@@ -101,7 +101,7 @@ module accountRouting {
         });
         app.post('/confirmResetPassword', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`confirmResetPassword, ${req.body.urlToken}`);
+                logger.logger.info(`confirmResetPassword, { urlToken : ${req.body.urlToken}}`);
                 await accountResetPassword.resetPassword(req.body.urlToken, req.body.password, res);
             } catch (error) {
                 next(error);
@@ -112,7 +112,7 @@ module accountRouting {
 
         app.post('/askIfNotAddFriend', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`askIfNotAddFriend, ${req.body.senderUsername}, ${req.body.receiverUsername}, ${req.body.sessionToken}`);
+                logger.logger.info(`askIfNotAddFriend, { senderUsername : ${req.body.senderUsername}, receiverUsername : ${req.body.receiverUsername}, sessionToken : ${req.body.sessionToken}}`);
                 if(await accountSignIn.checkSession(req.body.senderUsername, req.body.sessionToken, res)){
                     await accountFriends.askIfNotAddFriend(req.body.senderUsername, req.body.receiverUsername, res);
                 }else{
@@ -125,7 +125,7 @@ module accountRouting {
 
         app.post('/getFriends', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`getFriends, ${req.body.username}, ${req.body.itemsPerPage},  ${req.body.page}`);
+                logger.logger.info(`getFriends, { username : ${req.body.username}, itemsPerPage : ${req.body.itemsPerPage},  page : ${req.body.page}}`);
                 if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
                     await accountFriends.getFriends(req.body.username, req.body.itemsPerPage,  req.body.page, res);
                 }
@@ -140,7 +140,7 @@ module accountRouting {
         app.post('/getEnteringPendingFriendsRequests', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
 
             try {
-                logger.logger.info(`getEnteringPendingFriendsRequests, ${req.body.username}, ${req.body.itemsPerPage},  ${req.body.page}`);
+                logger.logger.info(`getEnteringPendingFriendsRequests, { username : ${req.body.username}, itemsPerPage : ${req.body.itemsPerPage}, page : ${req.body.page}}`);
                 if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
                     await accountFriends.getEnteringPendingFriendsRequests(req.body.username, req.body.itemsPerPage,  req.body.page, res);
                 }
@@ -154,7 +154,7 @@ module accountRouting {
 
         app.post('/getExitingPendingFriendsRequests', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`getExitingPendingFriendsRequests, ${req.body.username}, ${req.body.itemsPerPage},  ${req.body.page}`);
+                logger.logger.info(`getExitingPendingFriendsRequests, { username : ${req.body.username}, itemsPerPage : ${req.body.itemsPerPage}, page : ${req.body.page}}`);
                 if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
                     await accountFriends.getExitingPendingFriendsRequests(req.body.username, req.body.itemsPerPage,  req.body.page, res);
                 }
@@ -176,7 +176,7 @@ module accountRouting {
         });
         app.post('/blockUser', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`blockUser, ${req.body.username}, ${req.body.blockedUsername}`);
+                logger.logger.info(`blockUser, { username : ${req.body.username}, blockedUsername : ${req.body.blockedUsername}}`);
                 if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
                     await accountFriends.blockUser(req.body.username, req.body.blockedUsername, res);
                 }
@@ -190,7 +190,7 @@ module accountRouting {
 
         app.post('/unblockUser', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
             try {
-                logger.logger.info(`unblockUser, ${req.body.username}, ${req.body.blockedUsername}`);
+                logger.logger.info(`unblockUser, { username : ${req.body.username}, blockedUsername : ${req.body.blockedUsername}}`);
                 if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
                     await accountFriends.unblockUser(req.body.username, req.body.blockedUsername, res);
                 }
@@ -230,11 +230,11 @@ module accountRouting {
 
             socket.on('initSocketData', async (username: string, token: string): Promise<void> => {
                 try {
-                    logger.logger.info(`initSocketData, ${username}, ${token}`);
+                    logger.logger.info(`initSocketData, { username : ${username}, token : ${token}}`);
                     await accountNotification.initSocketData(socket, username, token);
                 } catch (error) {
                     logger.logger.error(error);
-                    socket.emit('error');(error);
+                    socket.emit('error');
                 }
             });
 
@@ -246,25 +246,25 @@ module accountRouting {
                     await accountNotification.synchronizeNotificationsWithSocket(socket);
                 } catch (error) {
                     logger.logger.error(error);
-                    socket.emit('error');(error);
+                    socket.emit('error');
                 }
             });
             socket.on('notificationIsSeen', async (id: string): Promise<void> => {
                 try {
-                    logger.logger.info(`notificationIsSeen, ${id}`);
+                    logger.logger.info(`notificationIsSeen, { id : ${id}}`);
                     await accountNotification.notificationIsSeen(socket, id);
                 } catch (error) {
                     logger.logger.error(error);
-                    socket.emit('error');(error);
+                    socket.emit('error');
                 }
             });
             socket.on('deleteNotification', async (id: string): Promise<void> => {
                 try {
-                    logger.logger.info(`deleteNotification, ${id}`);
+                    logger.logger.info(`deleteNotification, { id : ${id}}`);
                     await accountNotification.deleteNotification(socket, id);
                 } catch (error) {
                     logger.logger.error(error);
-                    socket.emit('error');(error);
+                    socket.emit('error');
                 }
             });
 
@@ -276,16 +276,16 @@ module accountRouting {
                     await accountFriends.getMessage(socket);
                 } catch (error) {
                     logger.logger.error(error);
-                    socket.emit('error');(error);
+                    socket.emit('error');
                 }
             });
             socket.on('sendMessage', async (username: string, message: string, date: Date): Promise<void> => {
                 try {
-                    logger.logger.info(`sendMessage, ${username}, ${message}, ${date}`);
+                    logger.logger.info(`sendMessage, { username : ${username}, message : ${message}, date : ${date}}`);
                     await accountFriends.sendMessage(username, message, date, socket);
                 } catch (error) {
                     logger.logger.error(error);
-                    socket.emit('error');(error);
+                    socket.emit('error');
                 }
             });
 
