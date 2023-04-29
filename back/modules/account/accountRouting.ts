9dbd967923dc1e9ada98bb62418cb62b545ff017
Tassadar921 <paul.lecuisinier@gmail.com>
2023-04-29 19:45:29 +0200

    try catch

--- back/modules/account/accountRouting.ts
+++ back/modules/account/accountRouting.ts
@@ -5,7 +5,7 @@
 //1.0.0 - 15/03/2023 - Iémélian RAMBEAU - Creation of the first version
 //--------------------------------------------------------------------------------------
 
-import {Express, Request, Response} from 'express';
+import {Express, NextFunction, Request, Response} from 'express';
 import * as socketIO from 'socket.io';
 
 import socketOptions from '../common/socket/socketOptions';
@@ -36,78 +36,137 @@ module accountRouting {
 
         /*----------------------------------------SignUp----------------------------------------*/
 
-        app.post('/signUp', async function (req: Request, res: Response): Promise<void> {
-            await accountSignUp.createUserCreation(req.body.username, req.body.password, req.body.email, req.body.language, res);
+        app.post('/signUp', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`signUp, ${req.body.username}, ${req.body.password}, ${req.body.email}, ${req.body.language}`);
+                await accountSignUp.createUserCreation(req.body.username, req.body.password, req.body.email, req.body.language, res);
+            } catch (error) {
+                next(error);
+            }
         });
-        app.post('/confirmSignUp', async function (req: Request, res: Response): Promise<void> {
-            await accountSignUp.createUser(req.body.urlToken, res);
+
+        app.post('/confirmSignUp', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`confirmSignUp, ${req.body.urlToken}`);
+                await accountSignUp.createUser(req.body.urlToken, res);
+            } catch (error) {
+                next(error);
+            }
         });
 
         /*----------------------------------------Login----------------------------------------*/
 
-        app.post('/checkSession', async function (req: Request, res: Response): Promise<void> {
-            if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
-                await res.json({status: 1});
-            }
-            else{
-                await res.json({status: 0});
+        app.post('/checkSession', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`checkSession, ${req.body.username}, ${req.body.sessionToken}`);
+                if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
+                    await res.json({status: 1});
+                }
+                else{
+                    await res.json({status: 0});
+                }
+            } catch (error) {
+                next(error);
             }
         });
 
-        app.post('/signIn', async function (req: Request, res: Response): Promise<void> {
-            await accountSignIn.signIn(req.body.identifier, req.body.password, res);
+        app.post('/signIn', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`signIn, ${req.body.identifier}, ${req.body.password}`);
+                await accountSignIn.signIn(req.body.identifier, req.body.password, res);
+            } catch (error) {
+                next(error);
+            }
         });
 
-        app.post('/signOut', async function (req: Request, res: Response): Promise<void> {
-            await accountSignIn.signOut(req.body.username, req.body.sessionToken, res);
+
+        app.post('/signOut', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`signOut, ${req.body.username}, ${req.body.sessionToken}`)
+                await accountSignIn.signOut(req.body.username, req.body.sessionToken, res);
+            } catch (error) {
+                next(error);
+            }
         });
 
         /*----------------------------------------ResetPassword----------------------------------------*/
 
-        app.post('/resetPassword', async function (req: Request, res: Response): Promise<void> {
-            await accountResetPassword.mailResetPasswordCreateUrlToken(req.body.email, req.body.language, res);
+        app.post('/resetPassword', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`resetPassword, ${req.body.email}, ${req.body.language}`);
+                await accountResetPassword.mailResetPasswordCreateUrlToken(req.body.email, req.body.language, res);
+            } catch (error) {
+                next(error);
+            }
         });
-        app.post('/confirmResetPassword', async function (req: Request, res: Response): Promise<void> {
-            await accountResetPassword.resetPassword(req.body.urlToken, req.body.password, res);
+        app.post('/confirmResetPassword', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`confirmResetPassword, ${req.body.urlToken}`);
+                await accountResetPassword.resetPassword(req.body.urlToken, req.body.password, res);
+            } catch (error) {
+                next(error);
+            }
         });
 
         /*----------------------------------------Friends----------------------------------------*/
 
-        app.post('/askIfNotAddFriend', async function (req: Request, res: Response): Promise<void> {
-            if(await accountSignIn.checkSession(req.body.senderUsername, req.body.sessionToken, res)){
-                await accountFriends.askIfNotAddFriend(req.body.senderUsername, req.body.receiverUsername, res);
-            }else{
-                await res.json({status: 0});
+        app.post('/askIfNotAddFriend', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`askIfNotAddFriend, ${req.body.senderUsername}, ${req.body.receiverUsername}, ${req.body.sessionToken}`);
+                if(await accountSignIn.checkSession(req.body.senderUsername, req.body.sessionToken, res)){
+                    await accountFriends.askIfNotAddFriend(req.body.senderUsername, req.body.receiverUsername, res);
+                }else{
+                    await res.json({status: 0});
+                }
+            } catch (error) {
+                next(error);
             }
         });
 
-        app.post('/getFriends', async function (req: Request, res: Response): Promise<void> {
-            if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
-                await accountFriends.getFriends(req.body.username, req.body.itemsPerPage,  req.body.page, res);
-            }
-            else{
-                await res.json({status: 0});
-            }
-        });
-
-        app.post('/getEnteringPendingFriendsRequests', async function (req: Request, res: Response): Promise<void> {
-            if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
-                await accountFriends.getEnteringPendingFriendsRequests(req.body.username, req.body.itemsPerPage,  req.body.page, res);
-            }
-            else{
-                await res.json({status: 0});
+        app.post('/getFriends', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`getFriends, ${req.body.username}, ${req.body.itemsPerPage},  ${req.body.page}`);
+                if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
+                    await accountFriends.getFriends(req.body.username, req.body.itemsPerPage,  req.body.page, res);
+                }
+                else{
+                    await res.json({status: 0});
+                }
+            } catch (error) {
+                next(error);
             }
         });
 
-        app.post('/getExitingPendingFriendsRequests', async function (req: Request, res: Response): Promise<void> {
-            if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
-                await accountFriends.getExitingPendingFriendsRequests(req.body.username, req.body.itemsPerPage,  req.body.page, res);
-            }
-            else{
-                await res.json({status: 0});
+        app.post('/getEnteringPendingFriendsRequests', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+
+            try {
+                logger.logger.info(`getEnteringPendingFriendsRequests, ${req.body.username}, ${req.body.itemsPerPage},  ${req.body.page}`);
+                if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
+                    await accountFriends.getEnteringPendingFriendsRequests(req.body.username, req.body.itemsPerPage,  req.body.page, res);
+                }
+                else{
+                    await res.json({status: 0});
+                }
+            } catch (error) {
+                next(error);
             }
         });
 
+        app.post('/getExitingPendingFriendsRequests', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`getExitingPendingFriendsRequests, ${req.body.username}, ${req.body.itemsPerPage},  ${req.body.page}`);
+                if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
+                    await accountFriends.getExitingPendingFriendsRequests(req.body.username, req.body.itemsPerPage,  req.body.page, res);
+                }
+                else{
+                    await res.json({status: 0});
+                }
+            } catch (error) {
+                next(error);
+            }
+        });
+
+<<<<<<< HEAD
         app.post('/getOtherUsers', async function (req: Request, res: Response): Promise<void> {
             if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
                 await accountFriends.getOtherUsers(req.body.username, req.body.itemsPerPage,  req.body.page, res);
@@ -117,12 +176,48 @@ module accountRouting {
             }
         });
 
+=======
+        app.post('/blockUser', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`blockUser, ${req.body.username}, ${req.body.blockedUsername}`);
+                if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
+                    await accountFriends.blockUser(req.body.username, req.body.blockedUsername, res);
+                }
+                else{
+                    await res.json({status: 0});
+                }
+            } catch (error) {
+                next(error);
+            }
+        });
+
+        app.post('/unblockUser', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                logger.logger.info(`unblockUser, ${req.body.username}, ${req.body.blockedUsername}`);
+                if(await accountSignIn.checkSession(req.body.username, req.body.sessionToken, res)){
+                    await accountFriends.unblockUser(req.body.username, req.body.blockedUsername, res);
+                }
+                else{
+                    await res.json({status: 0});
+                }
+            } catch (error) {
+                next(error);
+            }
+        });
+
+
+
+>>>>>>> 9e3cf8bfdd772075feea722f13ed8ae8b1e4a33e
         /*--------------------------------------Notification-------------------------------------*/
 
 
-        app.post('/test', async function (req: Request, res: Response): Promise<void> {
-            //code
-            await res.json({status: 1});
+        app.post('/test', async function (req: Request, res: Response, next : NextFunction): Promise<void> {
+            try {
+                //code
+                await res.json({status: 1});
+            } catch (error) {
+                next(error);
+            }
         });
 
         return;
@@ -136,39 +231,74 @@ module accountRouting {
         const accountFriends: AccountFriends = new AccountFriends(accountNotification);
 
         io.on('connection', (socket: socketIO.Socket<any>): void => {
-            logger.logger.info('-----  New client connected    -----')
+            logger.logger.info('-----  New client connected    -----');
             socket.emit('initSocketData');
 
             socket.on('initSocketData', async (username: string, token: string): Promise<void> => {
-                await accountNotification.initSocketData(socket, username, token);
+                try {
+                    logger.logger.info(`initSocketData, ${username}, ${token}`);
+                    await accountNotification.initSocketData(socket, username, token);
+                } catch (error) {
+                    logger.logger.error(error);
+                    socket.emit('error');(error);
+                }
             });
 
             /*------------------------------------Notification------------------------------------*/
 
             socket.on('synchronizeNotifications', async (): Promise<void> => {
-                await accountNotification.synchronizeNotificationsWithSocket(socket);
+                try {
+                    logger.logger.info('synchronizeNotifications');
+                    await accountNotification.synchronizeNotificationsWithSocket(socket);
+                } catch (error) {
+                    logger.logger.error(error);
+                    socket.emit('error');(error);
+                }
             });
             socket.on('notificationIsSeen', async (id: string): Promise<void> => {
-                await accountNotification.notificationIsSeen(socket, id);
+                try {
+                    logger.logger.info(`notificationIsSeen, ${id}`);
+                    await accountNotification.notificationIsSeen(socket, id);
+                } catch (error) {
+                    logger.logger.error(error);
+                    socket.emit('error');(error);
+                }
             });
             socket.on('deleteNotification', async (id: string): Promise<void> => {
-                await accountNotification.deleteNotification(socket, id);
+                try {
+                    logger.logger.info(`deleteNotification, ${id}`);
+                    await accountNotification.deleteNotification(socket, id);
+                } catch (error) {
+                    logger.logger.error(error);
+                    socket.emit('error');(error);
+                }
             });
 
             /*----------------------------------------Chat----------------------------------------*/
 
             socket.on('getChat', async (): Promise<void> => {
-                await accountFriends.getMessage(socket);
+                try {
+                    logger.logger.info('getChat');
+                    await accountFriends.getMessage(socket);
+                } catch (error) {
+                    logger.logger.error(error);
+                    socket.emit('error');(error);
+                }
             });
             socket.on('sendMessage', async (username: string, message: string, date: Date): Promise<void> => {
-                await accountFriends.sendMessage(username, message, date, socket);
+                try {
+                    logger.logger.info(`sendMessage, ${username}, ${message}, ${date}`);
+                    await accountFriends.sendMessage(username, message, date, socket);
+                } catch (error) {
+                    logger.logger.error(error);
+                    socket.emit('error');(error);
+                }
             });
 
             socket.on('disconnect', async (): Promise<void> => {
-                logger.logger.info('client déconnecté')
+                logger.logger.info('client déconnecté');
             });
         });
-
         return;
     }
 }
